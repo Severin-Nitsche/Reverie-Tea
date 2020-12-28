f1480953c641233fc214ed65411324d6a4aa33ec
Severin-Nitsche <severinnitsche@gmail.com>
2020-12-28 22:49:14 +0100

    Corrected Empty to Unit. Removed thought Comments. Added TypeVariables to other types, so it is clear where they get introducedin the program.

--- src/com.github.severinnitsche.MilnerTypes/com/github/severinnitsche/milner_types/TypeRep.java
+++ src/com.github.severinnitsche.MilnerTypes/com/github/severinnitsche/milner_types/TypeRep.java
@@ -7,13 +7,7 @@ public sealed interface TypeRep permits TypeRep.Algebraic, TypeRep.Func, TypeRep
   public record TypeVariable(String identifier, List<Algebraic> constraints) implements TypeRep {
 
   }
-  //List = Nil | Cons a
-  //Cons = a + List
-
-  //List = TypeRep.Algebraic.Sum([],[Nil,Cons a])
-  //Nil = TypeRep.Algebraic.Empty([])
-  //Cons = TypeRep.Algebraic.Product([],[a,List])
-  public sealed interface Algebraic extends TypeRep permits Algebraic.Sum, Algebraic.Product, Algebraic.Empty {
+  public sealed interface Algebraic extends TypeRep permits Algebraic.Sum, Algebraic.Product, Algebraic.Unit {
     List<Algebraic> directSuperTypes();
 
     private static List<Algebraic> _superTypes(List<Algebraic> types) {
@@ -23,21 +17,21 @@ public sealed interface TypeRep permits TypeRep.Algebraic, TypeRep.Func, TypeRep
     default List<Algebraic> superTypes() {
       return _superTypes(directSuperTypes());
     }
-    public record Empty(List<Algebraic> directSuperTypes) implements Algebraic {
+    public record Unit(List<Algebraic> directSuperTypes, List<TypeVariable> typeVariable) implements Algebraic {
 
     }
-    public record Sum(List<Algebraic> directSuperTypes, List<TypeRep> sum) implements Algebraic {
+    public record Sum(List<Algebraic> directSuperTypes, List<TypeVariable> typeVariable, List<TypeRep> sum) implements Algebraic {
 
     }
-    public record Product(List<Algebraic> directSuperTypes, List<TypeRep> product) implements Algebraic {
+    public record Product(List<Algebraic> directSuperTypes, List<TypeVariable> typeVariable, List<TypeRep> product) implements Algebraic {
 
     }
   }
   public sealed interface Func extends TypeRep permits Func.Function, Func.Method {
-    public record Function(List<TypeRep> in, List<TypeRep> out) implements Func {
+    public record Function(List<TypeRep> in, List<TypeRep> out, List<TypeVariable> typeVariable) implements Func {
 
     }
-    public record Method(Algebraic in, Function out) implements Func {
+    public record Method(Algebraic in, Function out, List<TypeVariable> typeVariable) implements Func {
 
     }
   }
